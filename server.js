96d714e0d28acbe2f3b357e8a42d206a72719284
lukeperry <lukezabala@gmail.com>
2025-01-08 13:52:28 +0800

    moddified production connections

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.get("*", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
