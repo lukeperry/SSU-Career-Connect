d1d6c27a082c54335250755e8fbc8b73f9522406
lukeperry <lukezabala@gmail.com>
2025-01-08 15:09:25 +0800

    commit

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.get("*", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
